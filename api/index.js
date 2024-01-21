2c14a39f8d8d24ba156ea004484c9c268f3f87e9
Rishank01 <rishankmehrotra4@gmail.com>
2024-01-21 14:38:24 +0530

    creation of server and connection to DB

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express from 'express'; // Since we are using the import express from express , So we must use the type as module in package.json
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+// Database connection
+mongoose.connect(process.env.DB_URL)
+.then(() => {
+    console.log("DB Connection Successfull");
+})
+.catch((error) => {
+    console.log("Error while connecting to the DB" , error);
+}) 
+
+
+const app = express();
+
+// Listening the Server
+app.listen(3000 , () => {
+    console.log("Server is running on the port number 3000");
+});
