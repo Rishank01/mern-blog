3b3222b89297402c1ac0fa4b0e22fc9cdf33ea47
Rishank01 <rishankmehrotra4@gmail.com>
2024-01-25 23:07:17 +0530

    update Header component with User Data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import React from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
 
-    const path = useLocation().pathname;
+  const {currentUser} = useSelector(state => state.user);
+  const path = useLocation().pathname;
 
   return (
     <Navbar className='border-b-2'>
@@ -30,11 +32,25 @@ const Header = () => {
         <Button className='w-12 h-10 hidden sm:inline' color = 'gray' pill>
             <FaMoon/>
         </Button>
-        <Link to = '/signin'>
+
+        {/* Adding the Profile Photo in the Header */}
+        { currentUser ? 
+          (<Dropdown arrowIcon = {false} inline label = {<Avatar alt = 'user' img = {currentUser.profilePicture} rounded/>}>
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-mediumctruncate'>@{currentUser.email}</span>  
+            </Dropdown.Header>
+            <Link to = {'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider/>
+            <Dropdown.Item>Sign Out</Dropdown.Item>
+          </Dropdown>) : 
+        ( <Link to = '/signin'>
             <Button gradientDuoTone='purpleToBlue' outline>
                 Sign In
             </Button>
-        </Link>
+          </Link>) }    
 
         <Navbar.Toggle/> 
 
