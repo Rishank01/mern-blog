5e3cd098dc239213aad757423ca9f644e74f29cb
Rishank01 <rishankmehrotra4@gmail.com>
2024-01-27 13:29:31 +0530

    complete sidebar of the dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footercom from './components/Footercom';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 import About from './pages/About';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
@@ -17,7 +18,11 @@ const App = () => {
       <Routes>
         <Route path = '/' element = {<Home/>}/>
         <Route path = '/about' element = {<About/>}/>
-        <Route path = '/dashboard' element = {<Dashboard/>}/>
+
+        <Route element = {<PrivateRoute/>}>
+          <Route path = '/dashboard' element = {<Dashboard/>}/>
+        </Route>
+
         <Route path = '/signin' element = {<SignIn/>}/>
         <Route path = '/signup' element = {<SignUp/>}/>
         <Route path = '/projects' element = {<Projects/>}/>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashProfile from '../components/DashProfile';
+import DashSidebar from '../components/DashSidebar';
 
 const Dashboard = () => {
+
+  const [tab , setTab] = useState('');
+  const location = useLocation();
+  
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    // console.log(tabFromUrl);
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  } , [location.search]);
+
   return (
-    <div>
-      Dashboard
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashSidebar/>
+      </div>
+      
+      {/* Profile and other stuff... */}
+      {tab === 'profile' && (<DashProfile/>)}
     </div>
   )
 }
