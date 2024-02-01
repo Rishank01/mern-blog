77d9d3cdc67629d9ed9502cad6a0b22bf6e56c1f
Rishank01 <rishankmehrotra4@gmail.com>
2024-02-01 13:29:03 +0530

    complete create a post page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
 
 
 const App = () => {
@@ -21,6 +23,10 @@ const App = () => {
 
         <Route element = {<PrivateRoute/>}>
           <Route path = '/dashboard' element = {<Dashboard/>}/>
+        </Route>
+
+        <Route element = {<OnlyAdminPrivateRoute/>}>
+          <Route path = '/create-post' element = {<CreatePost/>}/>
         </Route>
 
         <Route path = '/signin' element = {<SignIn/>}/>
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,6 +5,7 @@ import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { useDispatch, useSelector } from 'react-redux';
 import { app } from '../firebase';
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, updateFailure, updateStart, updateSuccess , signoutSuccess } from '../redux/user/userSlice';
+import { Link } from 'react-router-dom';
 
 
 import { CircularProgressbar } from 'react-circular-progressbar';
@@ -16,7 +17,7 @@ export default function DashProfile() {
 
     const dispatch = useDispatch();
 
-    const {currentUser , error} = useSelector((state) => state.user);
+    const {currentUser , error , loading} = useSelector((state) => state.user);
     const [imageFile , setImageFile] = useState(null);
     const [imageFileUrl , setImageFileUrl] = useState(null);
     const [imageFileUploadProgress , setImageFileUploadProgress] = useState(null);
@@ -234,7 +235,20 @@ export default function DashProfile() {
         <TextInput type = 'email' id = 'email' placeholder='email' defaultValue = {currentUser.email} onChange={handleChange}/>
         <TextInput type = 'password' id = 'password' placeholder='password' onChange={handleChange}/>
 
-        <Button type = 'submit' gradientDuoTone='purpleToBlue' outline >Update</Button>
+        <Button type = 'submit' gradientDuoTone='purpleToBlue' outline disabled = {loading || imageFileUploading}>
+            {loading ? 'loading...' : 'Update'}
+        </Button>
+
+        {
+            currentUser.isAdmin && (
+                <Link to = {'/create-post'}>
+                    <Button type = 'button' gradientDuoTone='purpleToPink' className='w-full'>
+                        Create a post
+                    </Button>
+                </Link>
+            )
+        }
+
       </form>
       
       <div className='text-red-500 flex justify-between'>
