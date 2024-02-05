a64ad7c6d3eeaf8378de74dd29220486a19bbc85
Rishank01 <rishankmehrotra4@gmail.com>
2024-02-05 21:38:28 +0530

    create get users api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -92,3 +92,44 @@ export const signout = (req , res , next) => {
         next(error);
     }
 };
+
+
+export const getUsers =  async (req , res , next) => {
+    if(!req.user.isAdmin){
+        return next(errorHandler(403 , "You arr not allowed to see all users"));
+    }
+    try{
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+        const users = await User.find().sort({createdAt : sortDirection}).skip(startIndex).limit(limit); 
+
+        const usersWithoutPassword = users.map((user) => {
+            const {password , ...rest } = user._doc;            // Seperate all the users without password , So that we can access the users but not there passwords
+            return rest;
+        })
+
+        const totalUsers = await User.countDocuments();
+        // Last months users
+        const now = new Date();
+        const oneMonthAgo = new Date(
+            now.getFullYear(),      
+            now.getMonth()-1,
+            now.getDate()
+        );
+
+        const lastMonthUsers = await User.countDocuments({
+            createdAt : { $gte : oneMonthAgo }, // greater than one month ago
+        });
+
+        res.status(200).json({
+            users : usersWithoutPassword,
+            totalUsers , 
+            lastMonthUsers
+        });
+
+    }catch(error){
+        next(error);
+    }
+}
